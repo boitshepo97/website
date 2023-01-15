2d7b6154c692ce04112cb9db0ec2028aac9d5075
boitshepo97 <boitshepo.masemola@gmail.com>
2023-01-15 21:35:50 +0200

    New Website

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("about").addEventListener("click", function(){
+    document.getElementById("about").style.display = "block";
+  });
+  document.getElementById("projects").addEventListener("click", function(){
+    document.getElementById("projects").style.display = "block";
+  });
+  document.getElementById("cv").addEventListener("click", function(){
+    document.getElementById("cv").style.display = "block";
+  });
+  document.getElementById("contact").addEventListener("click", function(){
+    document.getElementById("contact").style.display = "block";
+  });
+  
